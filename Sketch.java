30c7d0c13612b2c1500e311efdcdfa9a42aac483
rachellehcar78 <95876157+rachellehcar78@users.noreply.github.com>
2022-01-04 22:50:01 -0500

    Update Sketch.java

--- Sketch.java
+++ Sketch.java
@@ -1,36 +1,53 @@
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
-	
-	
+
+    float scale = random (0, 500);
+
+    float petalX = scale/1;
+    float petalY = (float)(scale);
+    float petalSize = random(5, 200);
+    float petalDistance = petalSize/2;
+
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(500, 500);
   }
 
   /** 
    * Called once at the beginning of execution.  Add initial set up
    * values here i.e background, stroke, fill etc.
    */
-  public void setup() {
-    background(210, 255, 173);
+  public void setup() 
+  {
+    background(94, 218, 255);
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    // Petal #1
+    fill(255, 122, 235);
+    ellipse(petalX-petalDistance, petalY-petalDistance, petalSize, petalSize);
+
+    // Petal #2
+    fill(255, 122, 235);
+    ellipse(petalX + petalDistance, petalY - petalDistance, petalSize, petalSize);
+
+    // Petal #3
+    fill(255, 122, 235);
+    ellipse(petalX + petalDistance, petalY + petalDistance, petalSize, petalSize);
+
+    // Petal #4
+    fill(255, 122, 235);
+    ellipse(petalX - petalDistance, petalY + petalDistance, petalSize, petalSize);
+
+    // Middle Part
+    fill(255, 238, 56);
+    ellipse(petalX, petalY, petalSize, petalSize);}
   }
-  
-  // define other methods down here.
-}
